0308b53155928b5b8b8821c7b46b26729c4f0993
Ann Prikhnenko <vpliseikin@gmail.com>
2023-11-16 05:31:02 +0300

    Add implementation of lab5

--- scripts/lessons-script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $("#header").load("header.html", function() {
-        $('.header-burger').click(function(event) {
-            $('.header-nav').toggleClass('open-burger');
-        });
-    });
-    $("#footer").load("footer.html");
-});
