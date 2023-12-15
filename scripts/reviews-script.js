0b0002507d4173d5e15e7b583f18fe090dcb2449
Ann Prikhnenko <vpliseikin@gmail.com>
2023-12-16 01:52:53 +0300

    Add implementation of lab7

--- scripts/reviews-script.js
+++ scripts/reviews-script.js
@@ -6,3 +6,27 @@ $(function(){
     });
     $("#footer").load("../footer.html");
 });
+
+// Initializing a class Swiper:
+// '.swiper' - name of the default library class
+// You can make a more detailed setting in square brackets {} after '.swiper'
+new Swiper('.swiper', {
+    // Optional parameter loop:
+    // the last image/text switches to the first one
+    loop: true,
+
+    // Optional parameter pagination:
+    // there will be dots under the images/texts
+    // that will be highlighted in a different color
+    // when viewing a current image/text
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Optional parameter navigation:
+    // add arrows to switch images/texts
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
